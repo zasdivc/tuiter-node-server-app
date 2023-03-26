2debdeeed398b74f03b21ae9061467955e00d6f2
zasdivc <zasdivc@gmail.com>
2023-03-26 19:17:11 -0400

    second commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import HelloController
+  from "./controllers/hello-controller.js"
+import UserController
+  from "./controllers/users/users-controller.js"
+import TuitsController
+  from "./controllers/tuits/tuits-controller.js";
+import cors from 'cors';
+
+
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+HelloController(app)
+UserController(app)
+TuitsController(app);
+// app.get('/hello', (req, res) => {res.send('Life is good!')})
+// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.listen(process.env.PORT || 4000);
