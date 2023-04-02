5bdd67dc525586b8106f2ed39924c1c2ebb9eee3
zasdivc <zasdivc@gmail.com>
2023-04-02 08:06:25 -0400

    a2

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ import TuitsController
   from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors';
 
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
+
+
 
 
 const app = express();
